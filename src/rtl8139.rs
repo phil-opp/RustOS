56583716014ec3659cc7a98e248ede39b4bea366
Ryan Rasti <ryanrasti@gmail.com>
2014-11-19 21:34:59 -0800

    + can send (properly formed) udp/ip/ethernet packets!

--- src/rtl8139.rs
+++ src/rtl8139.rs
@@ -1,24 +1,30 @@
 // An attempt at an rtl8139 ethernet card driver
-use std::io::{Writer, IoResult, IoError};
+use std::io::IoResult;
 use arch::cpu::Port;
 use std::raw::Slice;
 use std::mem::transmute;
 
 pub struct Rtl8139 {
   command_register: Port, // TODO(ryan): better abstraction for registers (i.e., should take byte-width into consideration + also be for mmap)
-  transmit_address: Port,
-  transmit_status: Port,
-  config_1: Port
+  transmit_address: [Port,..4],
+  transmit_status: [Port,..4],
+  config_1: Port,
+  descriptor: uint
 }
 
 impl Rtl8139 { // TODO(ryan): is there already a frame oriented interface in std libs to implement?
 
   pub fn new(io_offset: u16) -> Rtl8139 {
     
-    Rtl8139 { config_1: Port::new(io_offset + 0x52),
-	      command_register: Port::new(io_offset + 0x37),
-	      transmit_address: Port::new(io_offset + 0x20),
-	      transmit_status: Port::new(io_offset + 0x10)
+    let p = |off: u16| -> Port {
+      Port::new(io_offset + off)
+    };
+    
+    Rtl8139 { config_1: p(0x52),
+	      command_register: p(0x37),
+	      transmit_address: [p(0x20), p(0x24), p(0x28), p(0x2c)],
+	      transmit_status: [p(0x10), p(0x14), p(0x18), p(0x1c)],
+	      descriptor: 0
 	      }
   }
 
@@ -34,21 +40,19 @@ impl Rtl8139 { // TODO(ryan): is there already a frame oriented interface in std
     
   }
   
-  pub fn put_frame(&mut self, bytes: &[u8]) -> bool {
+  pub fn put_frame(&mut self, bytes: &[u8]) -> IoResult<()> {
     let slice_bytes: Slice<u8> = unsafe { transmute(bytes) };
 
     debug!("sending {} bytes", slice_bytes.len)
     
 
-    self.transmit_address.out_l(slice_bytes.data as u32);
+    self.transmit_address[self.descriptor].out_l(slice_bytes.data as u32);
 
-    debug!("transmit_status before send is 0x{:x}", self.transmit_status.in_l())
-    self.transmit_status.out_l(0xfff & (slice_bytes.len as u32));
-    debug!("transmit_status after send is 0x{:x}", self.transmit_status.in_l())
+    self.transmit_status[self.descriptor].out_l(0xfff & (slice_bytes.len as u32));
     
-    while (self.transmit_status.in_l() & 0x8000 == 0) { }
-    
-    true
+    while (self.transmit_status[self.descriptor].in_l() & 0x8000 == 0) { } // TODO(ryan): this is fragile if error sending...
+    self.descriptor = (self.descriptor + 1) % 4;
+    Ok(())
   }
 
 }
